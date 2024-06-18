54a075648625db4da77489983706eba115f5b998
Thomas Manley <tmanley1985@gmail.com>
2024-06-17 21:05:36 -0400

    Taking an example from another library so I can reverse engineer it

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/tmanley1985/gokafka
 
 go 1.22.2
 
-require github.com/confluentinc/confluent-kafka-go/v2 v2.4.0 // indirect
+require github.com/confluentinc/confluent-kafka-go/v2 v2.4.0
--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"log/slog"
 	"net"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 type Message struct {
@@ -62,6 +66,84 @@ func (s *Server) handleConn(conn net.Conn) {
 
 func main() {
 	server := NewServer()
+	go func ()  {
+		log.Fatal(server.Listen())
+	}()
 
-	server.Listen()
+	time.Sleep(time.Second)
+
+	// fmt.Println("consuming...")
+	// consume()
+
+	fmt.Println("producing...")
+	produce()
+}
+
+
+func produce() error {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost:9092"})
+	if err != nil {
+		return err
+	}
+
+	defer p.Close()
+
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	// Produce messages to topic (asynchronously)
+	topic := "someTopic"
+	for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
+		p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(word),
+		}, nil)
+	}
+
+	// Wait for message deliveries before shutting down
+	p.Flush(15 * 1000)
+
+	return nil
+}
+
+func consume()  error {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		return err
+	}
+
+	c.SubscribeTopics([]string{"someTopic"}, nil)
+
+	// A signal handler or similar could be used to set this to false to break the loop.
+	run := true
+
+	for run {
+		msg, err := c.ReadMessage(time.Second)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		} else if !err.(kafka.Error).IsTimeout() {
+			// The client will automatically try to recover from all errors.
+			// Timeout is not considered an error because it is raised by
+			// ReadMessage in absence of messages.
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
+
+	return c.Close()
 }
